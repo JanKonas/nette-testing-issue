949035e72a11301af5e0b57494f87c9ad64375e1
Jan Konáš <jan.konas@gmail.com>
2016-12-15 15:33:27 +0100

    Subscribed to postLoad event

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -3,18 +3,24 @@
 namespace App\Presenters;
 
 use App\Entity\Article;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Events;
 use Kdyby\Doctrine\EntityManager;
 use Nette;
 
 
-class HomepagePresenter extends Nette\Application\UI\Presenter implements
+class HomepagePresenter extends Nette\Application\UI\Presenter implements EventSubscriber
 {
 
 	private $entityManager;
 
+	private $postLoadCalled = false;
+
 	public function __construct(EntityManager $entityManager)
 	{
 		parent::__construct();
+		$entityManager->getEventManager()->addEventSubscriber($this);
 		$this->entityManager = $entityManager;
 	}
 
@@ -25,6 +31,17 @@ class HomepagePresenter extends Nette\Application\UI\Presenter implements
 		$this->entityManager->flush();
 
 		$this->template->article = $this->entityManager->getRepository(Article::class)->findOneBy([]);
+		$this->template->postLoadCalled = $this->postLoadCalled;
+	}
+
+	public function getSubscribedEvents()
+	{
+		return [Events::postLoad];
+	}
+
+	public function postLoad(LifecycleEventArgs $args)
+	{
+		$this->postLoadCalled = true;
 	}
 
 }
